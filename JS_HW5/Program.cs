394b4ede26b5104d521e23fa28770ec8ae2131f1
Arman KZ <armansauyenov@gmail.com>
2018-12-12 16:46:21 +0600

    Добавьте файлы проекта.

---
+++ JS_HW5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JS_HW5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
